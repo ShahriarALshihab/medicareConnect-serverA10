fb59faa79325fdc531becefff57d283b832d564f
ShahriarAlShihab <shahriarshihab96@gmail.com>
2026-06-25 22:46:33 +0600

    updated

--- index.js
+++ index.js
@@ -19,21 +19,21 @@ initFirebaseAdmin();
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL || "http://localhost:3000"],
+    origin: [
+      process.env.CLIENT_URL || "https://medicare-client-a10.vercel.app",
+    ],
     credentials: true,
-  })
+  }),
 );
 app.use(express.json());
-
 
 app.get("/", (req, res) => {
   res.send("MediCare Connect API is running ✅");
 });
 
-app.use("/", authRoutes); 
+app.use("/", authRoutes);
 app.use("/users", userRoutes);
 app.use("/doctors", doctorRoutes);
 app.use("/appointments", appointmentRoutes);
@@ -42,11 +42,9 @@ app.use("/payments", paymentRoutes);
 app.use("/prescriptions", prescriptionRoutes);
 app.use("/stats", statsRoutes);
 
-
 app.use((req, res) => {
   res.status(404).send({ message: "Route not found" });
 });
-
 
 app.use((err, req, res, next) => {
   console.error(err);
